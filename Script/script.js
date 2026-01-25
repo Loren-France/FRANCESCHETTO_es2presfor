5a63f08778d64863c53b3f5275c77371e38d1f90
Loren-France <14529846@itisrossi.vi.it>
2026-01-25 15:46:23 +0100

    Definitivo

--- Script/script.js
+++ Script/script.js
@@ -1,34 +1,17 @@
+let turni = new Map([
+    ["Turno 08:00-09:00", ["S01", "S02", "S03"]],
+    ["Turno 09:00-10:00", ["S04", "S05", "S06"]],
+    ["Turno 10:00-11:00", ["S07", "S08", "S09"]]
+]);
+
 function CreaTurni() {
 document.getElementById("tabellaTurni").innerHTML = "";
 document.getElementById("output").innerHTML = "";
 document.getElementById("operazioni").innerHTML = "";
 
-    let turni = new Map([
-        ["Turno 08:00-09:00", ["S01", "S02", "S03"]],
-        ["Turno 09:00-10:00", ["S04", "S05", "S06"]],
-        ["Turno 10:00-11:00", ["S07", "S08", "S09"]]
-    ]);
+    //Creazione mappa dei turni
 
     stampaTurni(turni);
-
-    let quantTurni = turni.size;
-
-    let quantStudenti = 0;
-    for (let [chiave,valore] of turni) {
-        quantStudenti += valore.length;
-    }
-
-    if(quantTurni<=0 || quantStudenti<=0){
-        document.getElementById("output").innerHTML = "Non sono presenti ne turni ne studenti!";
-    } else if (quantTurni<=0) {
-        document.getElementById("output").innerHTML = "Non sono presenti turni!";
-    } else if (quantStudenti<=0) {
-        document.getElementById("output").innerHTML = "Non sono presenti studenti!";
-    } else {
-        document.getElementById("output").innerHTML = "Numero totale di turni: "+ quantTurni;
-        document.getElementById("output").innerHTML += "<br><br>";
-        document.getElementById("output").innerHTML += "Numero totale di studenti: "+ quantStudenti;
-    }
 
     //Creazione sezione operazioni sugli orari del laboratorio
 
@@ -64,6 +47,10 @@ document.getElementById("operazioni").innerHTML = "";
     ricerca = document.createElement("div");
     ricerca.id = "outputRicerca";
     appendi.appendChild(ricerca);
+
+    for (let i = 0; i < 2; i++) {
+        appendi.appendChild(document.createElement("br"));
+    }
 
     //Creazione input, output e bottone per l'AGGIUNTA di uno studente
 
@@ -105,6 +92,10 @@ document.getElementById("operazioni").innerHTML = "";
     aggiunta = document.createElement("div");
     aggiunta.id = "outputAggiunta";
     appendi.appendChild(aggiunta);
+
+    for (let i = 0; i < 2; i++) {
+        appendi.appendChild(document.createElement("br"));
+    }
 
     //Creazione input, output e bottone per la RIMOZIONE di uno studente
 
@@ -149,16 +140,66 @@ document.getElementById("operazioni").innerHTML = "";
 
     for (let i = 0; i < 2; i++) {
         appendi.appendChild(document.createElement("br"));
-    }    
+    }
 }
 
 function stampaTurni(turni) {
     let tabella = document.getElementById("tabellaTurni");
+    let cont = [];
     tabella.innerHTML = "<h2>Tabella dei turni di accesso al laboratorio</h2><br>";
 
     for (let [chiave, valore] of turni) {
         tabella.innerHTML += chiave + " : " + valore.join(", ") + "<br><br>";
     }
+
+    let quantTurni = turni.size;
+
+    let quantStudenti = 0;
+    for (let [chiave,valore] of turni) {
+        quantStudenti += valore.length;
+        cont.push(valore.length);
+    }
+
+    if(quantTurni<=0 || quantStudenti<=0){
+        document.getElementById("output").innerHTML = "Non sono presenti ne turni ne studenti!";
+    } else if (quantTurni<=0) {
+        document.getElementById("output").innerHTML = "Non sono presenti turni!";
+    } else if (quantStudenti<=0) {
+        document.getElementById("output").innerHTML = "Non sono presenti studenti!";
+    } else {
+        document.getElementById("output").innerHTML = "Numero totale di turni: "+ quantTurni;
+        document.getElementById("output").innerHTML += "<br><br>";
+        document.getElementById("output").innerHTML += "Numero totale di studenti: "+ quantStudenti;
+    }
+
+    if (cont[0] == cont[1] && cont[0] == cont[2]) {
+        document.getElementById("output").innerHTML += "<br><br>Il numero di studenti è uguale in tutti i turni";
+    } else if (cont[0] > cont[1] && cont[0] > cont[2]) {
+        document.getElementById("output").innerHTML += "<br><br>Il turno con più studenti è il Turno 08:00-09:00";
+    } else if (cont[1] > cont[0] && cont[1] > cont[2]) {
+        document.getElementById("output").innerHTML += "<br><br>Il turno con più studenti è il Turno 09:00-10:00";
+    } else {
+        document.getElementById("output").innerHTML += "<br><br>Il turno con più studenti è il Turno 10:00-11:00";
+    }
+
+    document.getElementById("output").innerHTML += "<br><br>Turni con almeno 4 studenti: ";
+    document.getElementById("output").innerHTML += "<ul>";
+
+    if (cont[0] >= 4) {
+        document.getElementById("output").innerHTML += "<li>Turno 08:00-09:00</li>";
+    }
+    if (cont[1] >= 4) {
+        document.getElementById("output").innerHTML += "<li>Turno 09:00-10:00</li>";
+    }
+    if (cont[2] >= 4) {
+        document.getElementById("output").innerHTML += "<li>Turno 10:00-11:00</li>";
+    }
+    if (cont[0] < 4 && cont[1] < 4 && cont[2] < 4) {
+        document.getElementById("output").innerHTML += "<li>Nessun turno ha più di 4 studenti</li>";
+    }
+    document.getElementById("output").innerHTML += "</ul>";
+
+    return;
 }
 
 
@@ -188,16 +229,18 @@ function aggiuntaStudente(turni) {
     let aggiunto = document.getElementById("studenteAggiunta").value;
     let turno = document.getElementById("turnoAggiunta").value;
 
+    for(let [chiave,valore] of turni) {
+        if(valore.includes(aggiunto)){
+            document.getElementById("outputAggiunta").innerHTML = "Lo studente " + aggiunto + " è già presente nel " + chiave;
+            return;
+        }
+    }
+
+
     if (turni.has(turno)) {
 
         let array = turni.get(turno);
 
-        for (let i = 0; i < array.length; i++) {
-            if (array[i] == aggiunto) {
-                document.getElementById("outputAggiunta").innerHTML = "Lo studente " + aggiunto + " è già presente nel " + turno + "!";
-                return;
-            }
-        }
         array.push(aggiunto);
 
         turni.set(turno, array);
